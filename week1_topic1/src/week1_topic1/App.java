663412462844dfb959681c200c14a5326cc0c11c
jgarcia-network <3106189+jgarcia-network@users.noreply.github.com>
2020-04-14 13:17:15 -0700

    complete

--- week1_topic1/src/week1_topic1/App.java
+++ week1_topic1/src/week1_topic1/App.java
@@ -18,7 +18,25 @@ public class App {
 	
 		System.out.printf("Money left in my wallet is $%.2f\n" , change);
 		System.out.println("Number of friends I've made each year is " + numberFriends);
-		System.out.println("My full name is " + fullName);
+		System.out.println("My full name is " + fullName + System.lineSeparator());
+		
+		//second set
+		
+		double salePrice = 10.70;
+		double slushFunds = 500.00;
+		int colleagues = 5;
+		int ageYear = 20;
+		String studentName = "Jesus";
+		String studentLastname = "Garcia";
+		char middleInitial = 'L';
+		
+		double surplus = slushFunds - salePrice;
+		int numberColleagues = ageYear + colleagues;
+		String studentFullname = studentName + " " + middleInitial + " " + studentLastname;
+	
+		System.out.printf("Money left in my wallet is $%.2f\n" , surplus);
+		System.out.println("Number of colleauges I've had each year is " + numberColleagues);
+		System.out.println("My full student name is " + studentFullname);
 		
 	}
 
